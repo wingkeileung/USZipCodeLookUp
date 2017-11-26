f1a0c7bb026f58a5580e15bf564a2a5ae5bc2419
Wing Kei Leung <winky.leung@gmail.com>
2017-11-25 16:38:59 -0800

    Making progress to loop thru results of the post code/zip generated from City & State loop up

---
+++ src/components/CityStateInputForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="CityStateInputForm">
+    <form id="CityStateInput" v-on:submit="formSubmit">
+      <input type="text" v-model="cityToSubmit" placeholder="Enter a US City... e.g. (Seattle)">
+      <input type="text" v-model="stateToSubmit" placeholder="Enter the City State abbreviation... (e.g. WA)">
+      <input type="submit" value="lookup">
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CityStateInputForm',
+  data() {
+    return {
+      cityToSubmit:'',
+      stateToSubmit:''
+    }
+  },
+  methods: {
+    formSubmit (e) {
+      this.$emit('formSubmit', this.cityToSubmit, this.stateToSubmit);
+      e.preventDefault();
+    }
+  }
+}
+</script>
---
+++ src/components/ZipResult.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2 id="outputField">{{CityStateToZipResult}}</h2> 
+</template>
+
+<script>
+export default {
+  name: 'zipResult',
+  props: ['CityStateToZipResult']
+}
+</script>
