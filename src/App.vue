31a2587254576df65d8ec1792a72d778876ca50b
Wing Kei Leung <winky.leung@gmail.com>
2017-11-25 16:15:15 -0800

    Changing some Variable Names to prepare for more features

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,34 @@
     <h1>US Zipcode City reverse look up</h1>
     <h5>Powered by Vue.js</h5>
     <ZipInputForm v-on:formSubmit="cityResults" id="form"></ZipInputForm>
-    <CityResult v-text="apiResult['place name']"></CityResult>
-    <CityResult v-text="apiResult.state"></CityResult>
-    <CityResult v-text="apiResult['state abbreviation']"></CityResult>
-    <CityResult v-text="apiResult.longitude"></CityResult>
-    <CityResult v-text="apiResult.latitude"></CityResult>
+    <LocationResult v-text="zipToCityResult['place name']"></LocationResult>
+    <LocationResult v-text="zipToCityResult.state"></LocationResult>
+    <LocationResult v-text="zipToCityResult['state abbreviation']"></LocationResult>
+    <LocationResult v-text="zipToCityResult.longitude"></LocationResult>
+    <LocationResult v-text="zipToCityResult.latitude"></LocationResult>
   </div>
 </template>
 
 <script>
 import ZipInputForm from './components/ZipInputForm';
-import CityResult from './components/CityResult';
+import LocationResult from './components/LocationResult';
 
 export default {
   name: 'app',
   components: {
     ZipInputForm,
-    CityResult
+    LocationResult
   },
   data: function () {
     return {
-      apiResult:''
+      zipToCityResult:''
     }
   },
   methods: {
     cityResults:function(text){
       this.$http.get('http://api.zippopotam.us/us/'+text)
       .then((response) => {
-        this.apiResult = response.body.places[0]
+        this.zipToCityResult = response.body.places[0]
       });
     }
   }
---
+++ src/components/LocationResult.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2 id="outputField">{{zipToCityResult}}</h2> 
+</template>
+
+<script>
+export default {
+  name: 'LocationResult',
+  props: ['zipToCityResult']
+}
+</script>
